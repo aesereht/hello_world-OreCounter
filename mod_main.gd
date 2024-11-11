5bbdbc89f6f6596d133a86c3b67dcfc740cc8e9d
aesereht <100576755+aesereht@users.noreply.github.com>
2024-11-11 17:35:37 +0100

    text fix

--- mod_main.gd
+++ mod_main.gd
@@ -3,12 +3,12 @@ extends Node
 const MOD_NAME:String = "hello_world-OreCounter"
 
 func _init(_modLoader:Node = ModLoader) -> void :
-	ModLoaderLog.info("Init Timer.", MOD_NAME)
+	ModLoaderLog.info("Init Mod", MOD_NAME)
 	ModLoaderMod.add_translation("res://mods-unpacked/hello_world-OreCounter/translations/orecounter.en.translation")
 	ModLoaderMod.add_translation("res://mods-unpacked/hello_world-OreCounter/translations/orecounter.pl.translation")
 
 func _ready() -> void :
-	ModLoaderLog.info("Done", MOD_NAME)
+	ModLoaderLog.info("Mod Ready", MOD_NAME)
 	add_to_group("mod_init")
 
 func modInit() -> void : 
